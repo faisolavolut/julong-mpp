efc121cd6847c52bad4b45360d993160b62d5774
faisolavolut <faisol@avolut.com>
2024-12-27 14:58:07 +0700

    approval

--- src/app/d/location/[id]/view/page.tsx
+++ src/app/d/location/[id]/view/page.tsx
@@ -349,7 +349,7 @@ function Page() {
                   </ButtonContainer>
                 </Alert>
               )}
-              {local.can_reject && fm?.data?.status === "IN PROGRESS" ? (
+              {local.can_reject && fm?.data?.status === "NEED APPROVAL" && !fm?.data?.approver_manager_id ? (
                 <Alert
                   type={"save"}
                   content={
@@ -551,7 +551,7 @@ function Page() {
                 <></>
               )}
 
-              {local.can_approve && fm?.data?.status === "IN PROGRESS" ? (
+              {local.can_approve && fm?.data?.status === "NEED APPROVAL" && !fm?.data?.approver_manager_id ? (
                 <Alert
                   type={"save"}
                   content={
